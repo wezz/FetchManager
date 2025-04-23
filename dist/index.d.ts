b1023f9c0ab75d52b56b0c9eb2f613a1b17ea343
Johan Tor <johan.tor@gmail.com>
2025-04-23 08:57:33 +0200

    fix: build solution

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,43 @@
-export { default as FetchManager } from './FetchManager';
+declare class FetchManager {
+    moduleName: string;
+    private requestStore;
+    private defaultOptions;
+    private defaultFetchOptions;
+    constructor();
+    ObjToQueryString(params: any): string;
+    Fetch(initOptions: IFetchManagerOption): Promise<any>;
+    GetScript(url: string): Promise<unknown>;
+    CompileUrl(options: IFetchManagerOption): string;
+    private getRequestObj;
+    private getResponseFromCache;
+    private validResponse;
+    private saveResponseToCache;
+    private parseFetchOptions;
+    private getRequestCacheOptions;
+    private getCacheKey;
+    private getKey;
+    private debug;
+}
+export default FetchManager;
+
+declare interface IFetchManagerCacheOption {
+    usecache: boolean;
+    permanent: boolean;
+    pemanent: boolean;
+    cachekey: string;
+    iscached?: boolean;
+}
+
+declare interface IFetchManagerOption {
+    key?: string;
+    url: string;
+    querystring?: object | string;
+    requestdelay?: number;
+    cache?: boolean | IFetchManagerCacheOption;
+    fetchoptions?: RequestInit;
+    signal?: AbortController["signal"];
+    json?: boolean;
+    debug?: boolean;
+}
+
+export { }
